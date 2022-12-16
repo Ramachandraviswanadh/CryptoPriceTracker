04cea2da9d4b2be8b198b5ce6874a8c51d9f89b7
Rama Chandra Viswanadh <ramachandraviswanadh333@gmail.com>
2022-12-16 16:05:56 +0530

    landing started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import Header from "./components/Common/Header" 
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import DashboardPage from "./pages/dashboard";
+import CoinPage from "./pages/coin";
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
